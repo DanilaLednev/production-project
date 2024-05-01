eb3127b3acb96b71bb043dddd463fd2cfe28e606
Danila <mr.mozg.90@gmail.com>
2024-05-01 16:20:55 +0200

    Config for router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,11 @@
-import { Suspense, useContext, useState } from 'react'
+
 import { Link } from 'react-router-dom'
-import { Route, Routes } from 'react-router-dom'
 
 import './styles/index.scss'
 
-
 import { classNames } from 'shared/lib/classNames/classNames'
 import { useTheme } from 'app/providers/ThemeProvider'
-import { MainPage } from 'pages/mainPage'
-import { AboutPage } from 'pages/aboutPage'
+import { AppRouter } from './providers/router'
 
 
 
@@ -21,13 +18,7 @@ export const App = () => {
       <button onClick={ toggleTheme }>TOGGLER</button>
         <Link to="/">MAIN PAGE</Link>
         <Link to="/about">ABOUT PAGE</Link>
-
-    <Suspense fallback={ <div>Loading...</div> }>
-      <Routes>
-        <Route path={ '/' } element={ <MainPage /> } />
-        <Route path={ '/about' } element={ <AboutPage /> } />
-      </Routes>
-    </Suspense>
+        <AppRouter />
     </div>
   )
 }
---
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -0,0 +1,27 @@
+import { RouteProps } from "react-router-dom"
+
+import { MainPage } from "pages/mainPage";
+import { AboutPage } from "pages/aboutPage";
+
+
+
+export enum AppRoutes {
+  MAIN = 'main',
+  ABOUT = 'about'
+}
+
+export const RoutePath: Record<AppRoutes, string> = {
+ [AppRoutes.MAIN]: '/',
+ [AppRoutes.ABOUT]: '/about'
+}
+
+export const routeConfig: Record<AppRoutes, RouteProps> = {
+  [AppRoutes.MAIN]: {
+    path: RoutePath[AppRoutes.MAIN],
+    element: <MainPage />
+  },
+  [AppRoutes.ABOUT]: {
+    path: RoutePath[AppRoutes.ABOUT],
+    element: <AboutPage />
+  }
+}
