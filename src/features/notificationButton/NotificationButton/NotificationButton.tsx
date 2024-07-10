ccc643caf6023e33f38c0d397bdc589ba42536e2
Danila <mr.mozg.90@gmail.com>
2024-07-10 19:29:21 +0200

    add popover, refactoring

--- src/features/notificationButton/NotificationButton/NotificationButton.tsx
+++ src/features/notificationButton/NotificationButton/NotificationButton.tsx
@@ -1,6 +1,9 @@
+import { useCallback, useState } from 'react';
+import { BrowserView, MobileView } from 'react-device-detect';
 import { NotificationList } from 'entities/Notification';
 import BellIcon from 'shared/assets/icons/bell.svg';
 import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { Drawer } from 'shared/ui/Drawer/Drawer';
 import { Icon } from 'shared/ui/Icon/Icon';
 import { Popover } from 'shared/ui/Popups';
 import cls from './NotificationButton.module.scss';
@@ -12,14 +15,33 @@ interface NotificationButtonProps {
 export const NotificationButton = (props: NotificationButtonProps) => {
   const { className } = props;
 
+  const [isOpen, setIsOpen] = useState(false);
+  const onOpenDrawer = useCallback(() => {
+    setIsOpen(true);
+  }, []);
+  const onCloseDrawer = useCallback(() => {
+    setIsOpen(false);
+  }, []);
+
+  const trigger = (
+    <Button onClick={onOpenDrawer} theme={ButtonTheme.CLEAR}>
+      <Icon Svg={BellIcon} inverted />
+    </Button>
+  );
+
   return (
-    <Popover trigger={(
-      <Button theme={ButtonTheme.CLEAR}>
-        <Icon Svg={BellIcon} inverted />
-      </Button>
-    )}
-    >
-      <NotificationList className={cls.notification} />
-    </Popover>
+    <div>
+      <BrowserView>
+        <Popover trigger={trigger}>
+          <NotificationList className={cls.notification} />
+        </Popover>
+      </BrowserView>
+      <MobileView>
+        {trigger}
+        <Drawer isOpen={isOpen} onClose={onCloseDrawer}>
+          <NotificationList />
+        </Drawer>
+      </MobileView>
+    </div>
   );
 };
