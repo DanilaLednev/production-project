749720af4b65fcf7e4e2a47534303df3e432dbb0
Danila <mr.mozg.90@gmail.com>
2024-07-12 09:27:46 +0200

    add star rating, add Rating entity rating card

--- src/pages/mainPage/ui/MainPage.tsx
+++ src/pages/mainPage/ui/MainPage.tsx
@@ -1,6 +1,7 @@
 import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Page } from '@/widgets/Page/Page';
+import { RatingCard } from '@/entities/Rating/ui/RatingCard/RatingCard';
 
 const MainPage = memo(() => {
   const { t } = useTranslation();
@@ -9,6 +10,11 @@ const MainPage = memo(() => {
     <Page>
       {/* <BugButton /> */}
       {t('Главная страница')}
+      <RatingCard
+        title={t('Как вам статья')}
+        feedbackTitle={t('оставьте отзыв о статье')}
+        hasFeedback
+      />
     </Page>
   );
 });
---
+++ src/shared/ui/StarRating/StarRating.tsx
@@ -0,0 +1,70 @@
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import starIcon from '@/shared/assets/icons/star.svg';
+import { classNames } from '@/shared/lib/classNames/classNames';
+import { Icon } from '@/shared/ui/Icon/Icon';
+import cls from './StarRating.module.scss';
+
+interface StarRatingProps {
+  className?: string;
+  onSelect?: (starsCount: number) => void;
+  size?: number;
+  selectedStars?: number;
+}
+
+const stars = [1, 2, 3, 4, 5];
+
+export const StarRating = (props: StarRatingProps) => {
+  const {
+    className,
+    size = 30,
+    selectedStars = 0,
+    onSelect,
+  } = props;
+  const { t } = useTranslation();
+
+  const [isHovered, setHovered] = useState(false);
+  const [currentStarsCount, setCurrentStarsCount] = useState(0);
+  const [isSelected, setIsSelected] = useState(Boolean(selectedStars));
+
+  const onHover = (starsCount: number) => () => {
+    if (!isSelected) {
+      setCurrentStarsCount(starsCount);
+    }
+  };
+
+  const onLeave = () => {
+    if (!isSelected) {
+      setCurrentStarsCount(0);
+    }
+  };
+
+  const onClick = (starsCount: number) => () => {
+    if (!isSelected) {
+      onSelect?.(starsCount);
+      setCurrentStarsCount(starsCount);
+      setIsSelected(true);
+    }
+  };
+
+  return (
+    <div className={classNames(cls.StarRating, {}, [className])}>
+      {stars.map((starNumber, index) => (
+        <Icon
+          key={index}
+          className={classNames(
+            cls.StarIcon,
+            { [cls.selected]: isSelected },
+            [currentStarsCount >= starNumber ? cls.hovered : cls.normal],
+          )}
+          Svg={starIcon}
+          width={size}
+          height={size}
+          onMouseLeave={onLeave}
+          onMouseEnter={onHover(starNumber)}
+          onClick={onClick(starNumber)}
+        />
+      ))}
+    </div>
+  );
+};
