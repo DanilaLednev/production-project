d4e1889e50803f3fa4327574cb640f6d459a25cf
Danila <mr.mozg.90@gmail.com>
2024-07-13 14:38:17 +0200

    add feature profile rating card

---
+++ src/features/profileRating/ui/ProfileRating.tsx
@@ -0,0 +1,66 @@
+import { useCallback } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
+import { RatingCard } from '@/entities/Rating';
+import { getUserAuthData } from '@/entities/User';
+import { Skeleton } from '@/shared/ui/Skeleton/Skeleton';
+import { useGetProfileRating, useRateProfile } from '../api/profileRatingApi';
+
+interface ProfileRatingProps {
+  className?: string;
+  profileId: string;
+
+}
+
+export const ProfileRating = (props: ProfileRatingProps) => {
+  const { className, profileId } = props;
+  const { t } = useTranslation('profile');
+  const userData = useSelector(getUserAuthData);
+
+  const { data, isLoading } = useGetProfileRating({
+    profileId,
+    userId: userData?.id ?? '',
+  });
+
+  const [rateProfileMutation] = useRateProfile();
+
+  const rating = data?.[0];
+
+  const handleRateProfile = useCallback((starsCount: number, feedback?: string) => {
+    try {
+      rateProfileMutation({
+        userId: userData?.id ?? '',
+        profileId,
+        rate: starsCount,
+        feedback,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }, [profileId, rateProfileMutation, userData?.id]);
+
+  const onAccept = useCallback((starsCount: number, feedback?: string) => {
+    handleRateProfile(starsCount, feedback);
+  }, [handleRateProfile]);
+
+  const onCancel = useCallback((starsCount: number, feedback?: string) => {
+    handleRateProfile(starsCount);
+  }, [handleRateProfile]);
+
+  if (isLoading) {
+    return <Skeleton width="100%" height={120} />;
+  }
+
+  return (
+    <RatingCard
+      className={className}
+      rate={rating?.rate}
+      title={t('Оцените профиль')}
+      feedbackTitle={t('Оставьте свой отзыв о пользователе')}
+      hasFeedback
+      onCancel={onCancel}
+      onAccept={onAccept}
+    />
+
+  );
+};
--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router-dom';
 import { Page } from '@/widgets/Page/Page';
 import { EditableProfileCard } from '@/features/editableProfileCard';
+import { ProfileRating } from '@/features/profileRating';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { VStack } from '@/shared/ui/Stack/VStack/VStack';
 import { Text, TextAlign, TextTheme } from '@/shared/ui/Text/Text';
@@ -14,14 +15,17 @@ interface ProfilePageProps {
 const ProfilePage = memo(({ className }: ProfilePageProps) => {
   const { id } = useParams<{ id: string }>();
 
-  // if (!id) {
-  //   return <Text align={TextAlign.CENTER} theme={TextTheme.ERROR} title={t('Профиль не найден')} />;
-  // }
+  const { t } = useTranslation('profile');
+
+  if (!id) {
+    return <Text align={TextAlign.CENTER} theme={TextTheme.ERROR} title={t('Профиль не найден')} />;
+  }
 
   return (
     <Page className={classNames('', {}, [className])}>
       <VStack gap="16" max>
         <EditableProfileCard id={id} />
+        <ProfileRating profileId={id} />
       </VStack>
     </Page>
   );
