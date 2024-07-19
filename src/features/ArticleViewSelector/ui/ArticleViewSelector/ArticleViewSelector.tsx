e0c2346f231224c1b4f4a5292fe45394113b3875
Danila <mr.mozg.90@gmail.com>
2024-07-19 16:29:48 +0200

    add options in buildLoader for svg

--- src/features/ArticleViewSelector/ui/ArticleViewSelector/ArticleViewSelector.tsx
+++ src/features/ArticleViewSelector/ui/ArticleViewSelector/ArticleViewSelector.tsx
@@ -40,6 +40,8 @@ export const ArticleViewSelector = (props: ArticleViewSelectorProps) => {
           onClick={onClick(viewType.view)}
         >
           <Icon
+            width={24}
+            height={24}
             Svg={viewType.icon}
             className={classNames('', {
               [cls.notSelected]: viewType.view !== view,
